64dbf86d7e2cf7dba6dce70e528d532696cbc20d
Nok1sh <Nikita.Semenovykh@urfu.me>
2025-05-29 16:45:17 +0500

    Add files via upload

---
+++ BugForge_game/main.py
@@ -0,0 +1,22 @@
+from game_windows.game_handler import main_menu_loop
+
+def main():
+    import cProfile
+    import pstats
+
+    profiler = cProfile.Profile()
+    profiler.enable()
+    try:
+        main_menu_loop()
+    except:
+        pass
+
+    profiler.disable()
+
+    stats = pstats.Stats(profiler).sort_stats('tottime')
+    stats.print_stats()
+
+
+if __name__ == "__main__":
+    # main_menu_loop()
+    main()
