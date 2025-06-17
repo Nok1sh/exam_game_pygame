a37ce75f550b2feb99b7fd75351276eb31f08bc9
Nok1sh <Nikita.Semenovykh@urfu.me>
2025-06-18 02:09:37 +0500

    Update main.py

--- BugForge_game/main.py
+++ BugForge_game/main.py
@@ -3,23 +3,6 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 from windows.game_handler import main_menu_loop
 
-def main():
-    import cProfile
-    import pstats
-
-    profiler = cProfile.Profile()
-    profiler.enable()
-    try:
-        main_menu_loop()
-    except:
-        pass
-
-    profiler.disable()
-
-    stats = pstats.Stats(profiler).sort_stats('tottime')
-    stats.print_stats()
-
 
 if __name__ == "__main__":
     main_menu_loop()
-    #main()
